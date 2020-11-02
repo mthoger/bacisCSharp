0b74b2e910668cf2457eaa7da528eb008230dfcb
Admin <Admin@TA-826-11.kursdom.net>
2020-11-02 12:33:41 +0100

    Add project files.

---
+++ DebugDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DebugDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MTBacis1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MTBacis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
+            
+
+        }
+    }
+}
