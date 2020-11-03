f694550bd00f697a51f41998428f0d656891b4ba
Michael Thøgersen <michael_thogersen@hotmail.com>
2020-11-03 09:23:28 +0100

    added new opgave

---
+++ opgave481/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace opgave481
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kort1Værdi = 2;
+            KortKulør kort1Kulør = KortKulør.Hjerter;
+            Console.WriteLine($"{kort1Kulør.ToString()} {kort1Værdi}");
+            Console.WriteLine($"{Convert.ToInt32(kort1Kulør)} {kort1Værdi}");
+
+
+            int kort2Værdi = 2;
+            KortKulør kort2Kulør = KortKulør.Ruder;
+            Console.WriteLine($"{kort2Kulør.ToString()} {kort2Værdi}");
+            Console.WriteLine($"{Convert.ToInt32(kort2Kulør)} {kort2Værdi}");
+
+
+
+        }
+
+        internal enum KortKulør
+        {
+            Spar,
+            Hjerter,
+            Ruder,
+            Klør
+        }
+    }
+}
