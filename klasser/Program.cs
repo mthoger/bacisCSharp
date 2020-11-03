0a6fb360cae76095e8f7d75482663c95199f3161
Michael Thøgersen <michael_thogersen@hotmail.com>
2020-11-03 16:00:22 +0100

    klasser

---
+++ klasser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t = new Terning();
+            t = 5;
+        }
+
+        class Terning
+        {
+            private int værdi; 
+            //felter = værdier 
+            //egenskaber  = get/set indkaspling 
+            //metoder = functionalitet 
+            //hændelser =  actions
+        } 
+
+    }
+}
