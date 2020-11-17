537a00dbbbbef7cf0fad72f4fea710a259ea610a
Michael Thøgersen <michael_thogersen@hotmail.com>
2020-11-17 09:47:52 +0100

    advarv

---
+++ arvudvidet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom r = new UdvidetRandom();
+            Console.WriteLine(r.NextBool());
+            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class UdvidetRandom : System.Random
+    {
+        public bool NextBool()
+        {
+            int tal = this.Next(1, 1002);
+            bool res = tal < 501;
+            return res;
+        }
+    }
+}
